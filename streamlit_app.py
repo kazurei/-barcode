51ad63a76f50844d64684bb89ff2f7de813f6f73
kazurei <167180368+kazurei@users.noreply.github.com>
2025-02-25 08:45:00 +0000

    あいうえお

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,2 @@
-from io import BytesIO
-from barcode import Code128
-from barcode.writer import ImageWriter
-
-# Write to a file-like object:
-rv = BytesIO()
-Code128(str(100000902922), writer=ImageWriter()).write(rv)
-
-data = "AbCd-123456"
-fileName = data + ".jpeg"
-
-# Or to an actual file:
-with open(fileName, "wb") as f:
-    Code128(data, writer=ImageWriter()).write(f)
+import streamlit as st
+import barcode from 
